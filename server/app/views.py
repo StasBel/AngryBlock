6d67e7b809b3e4064e0b055b24276e7075198239
vitalik239 <vitalik239@me.com>
2015-03-23 00:30:37 +0300

    GET requests & format added

--- server/app/views.py
+++ server/app/views.py
@@ -1,15 +1,15 @@
-from app import app
-from flask import request
 
-@app.route('/ask', methods=['POST', 'GET'])
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import request, jsonify
+
+@app.route('/ask')
 def ask():
-	if request.method == 'POST':
-		if request.form['data'] == 'go home':
-			return 'u go'
-		else:
-			return request.form['data']
-	return 'Use POST request with [data] field'
-    
+	answer = [{'rate':(len(mess) % 2), 'text':mess} for mess in request.args.getlist('messages')]
+	print answer
+	return jsonify(estimates=answer)
+	
 @app.route('/index')
 def index():
 	return 'Welcome'
