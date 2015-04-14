57500f6beb29a5879af162685261b2c6dbb6f008
vitalik239 <vitalik239@me.com>
2015-04-14 11:32:10 +0300

    try this

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from flask.ext.cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, resources={r"/ask*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 from app import views
--- server/app/views.py
+++ server/app/views.py
@@ -2,11 +2,54 @@
 # -*- coding: utf-8 -*-
 
 from app import app
-from flask import request, jsonify
-from flask.ext.cors import cross_origin
+from flask.ext.cors import CORS, cross_origin
+from datetime import timedelta
+from flask import make_response, request, jsonify, current_app
+from functools import update_wrapper
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route('/ask', methods=['POST', 'GET'])
-@cross_origin()
+@crossdomain(origin='*')
 def ask():	
 	answer = []
 	for mess in request.form.getlist('messages'):
