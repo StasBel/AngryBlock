249bad728b3b66903d6e2aca65753fa3f1ad8a8a
vitalik239 <vitalik239@me.com>
2015-04-14 18:07:00 +0300

    json added

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request
 from flask.ext.cors import CORS, cross_origin
 
+
 app = Flask(__name__)
 cors = CORS(app, headers=['Content-Type'])
 
--- server/app/views.py
+++ server/app/views.py
@@ -3,17 +3,18 @@
 
 from app import app
 from flask import request, jsonify
+import json
 from flask.ext.cors import CORS, cross_origin
 
-@app.route('/ask', methods=['POST', 'GET'])
+@app.route('/ask', methods=['POST'])
 @cross_origin()
 def ask():	
 	answer = []
-	print request.form.getlist('messages')
-	for mess in request.form.getlist('messages'):
+	body = json.loads(request.data)
+	for mess in body['messages']:
 		answer.append({'rate':len(mess), 'text':mess})
 	return jsonify(estimates=answer)
 	
-@app.route('/index')
+@app.route('/')
 def index():
 	return 'Welcome'
