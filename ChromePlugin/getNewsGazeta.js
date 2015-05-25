0bdc68c2335d2f792f8dee643b89406fa0a8e789
Wowember <wowember@mail.ru>
2015-05-25 03:34:24 +0300

    add some comments, now on gazeta.spb.ru only repainted text

--- ChromePlugin/getNewsGazeta.js
+++ ChromePlugin/getNewsGazeta.js
@@ -1,6 +1,6 @@
 if (window.location.href == 'http://www.gazeta.spb.ru/allnews/')
 {
-    var main = document.getElementById('main')
+    var main = document.getElementById('main')   //Get news from page
     var divs = main.getElementsByTagName('div')
     var firstTable
     for (var i = 0; i < divs.length; i++)
@@ -14,41 +14,28 @@ if (window.location.href == 'http://www.gazeta.spb.ru/allnews/')
     var newsTable = firstTable.getElementsByTagName('div')[1]
 
     var messages = []
-    var textNews = newsTable.getElementsByTagName('a')
+    var textNews = newsTable.getElementsByTagName('a')   //Getting text of news
     for (var i = 0; i < textNews.length; i++)
     	messages[i] = textNews[i].innerHTML;
-
-
-
+   
     var news = newsTable.getElementsByTagName('div');
-    console.log(news.length);
+    
+
     var xhr = new XMLHttpRequest();
-    newsTable.removeChild(news[2]);
-    //xhr.open("POST", "http://192.168.210.5:33507/ask");
-    xhr.open("POST", "https://stormy-ravine-9552.herokuapp.com/ask");
+    xhr.open("POST", "https://stormy-ravine-9552.herokuapp.com/ask");   //Request to server
     xhr.timeout = 5000;
     var good = 0, bad = 0, del = 0;
     xhr.setRequestHeader("Content-Type", "text/plain");
-    xhr.onreadystatechange = function(){
-    	console.log(xhr.status);
+    xhr.onreadystatechange = function()
+    {
     	if (xhr.readyState == 4 && xhr.status == 200)
     	{
-    		var answer = JSON.parse(xhr.responseText);
-    		console.log(answer.estimates.length);
+    		var answer = JSON.parse(xhr.responseText);   //Accept and parse answer from server
     		for (var i = 0; i < answer.estimates.length; i++)
-    			if (answer.estimates[i].rate < 0.8)
-    			{
-    				//newsTable.removeChild(news[i * 2 - del]);
-    				bad++;
-    				del += 2;	
-    				textNews[i].style.color="red";
-    			}
-    			else
-    				good++;
-    		console.log(good);
-    		console.log(bad);
+    			if (answer.estimates[i].rate != 1)
+    				textNews[i].style.color="red";   //Highlight text in red
     	}	
     }   
 
-    xhr.send(JSON.stringify({messages: messages}));  
+    xhr.send(JSON.stringify({messages: messages}));   //Send news text 
 }
